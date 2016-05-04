93ee5399bfde623fc5ae551d53e1cd1d431a00a9
sairah814 <sairah814@gmail.com>
2016-05-03 21:56:21 -0400

    initial working version

---
+++ app.js
@@ -0,0 +1,14 @@
+angular.module('myApp', [])
+    .controller('MultiplicationCtrl', function ($scope, $attrs) {
+        $scope.numberLimit = $attrs.initialNumberLimit;
+        $scope.numbers = function () {
+            var numbers = [];
+            for (var i = 0; i < $scope.numberLimit; i++) {
+                numbers[i] = i + 1;
+            };
+            return numbers;
+        };
+        $scope.compute = function (a, b) {
+            return a * b;
+        };
+    });
